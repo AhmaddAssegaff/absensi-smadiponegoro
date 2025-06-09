76f61483a42245ed69f5feb19bed371c2a9bd9d9
AhmaddAssegaff <139890680+AhmaddAssegaff@users.noreply.github.com>
2025-06-09 19:14:55 +0700

    feat : attendance Summary page

---
+++ src/components/layout/chartPieLabel.tsx
@@ -0,0 +1,78 @@
+import { Pie, PieChart, Legend } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+type PieChartData = {
+  name: string;
+  value: number;
+  fill?: string;
+};
+
+type ChartPieLabelProps = {
+  title?: string;
+  description?: string;
+  data: PieChartData[];
+  footerChartTitle?: string;
+  footerChartDescription?: string;
+};
+
+export function ChartPieLabel({
+  title = "Pie Chart",
+  description,
+  data,
+  footerChartDescription,
+  footerChartTitle,
+}: ChartPieLabelProps) {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0 text-center">
+        <CardTitle>{title}</CardTitle>
+        {description && (
+          <CardDescription className="text-sm text-muted-foreground">
+            {description}
+          </CardDescription>
+        )}
+      </CardHeader>
+
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={{}}
+          className="mx-auto aspect-square w-full max-w-[400px] [&_.recharts-pie-label-text]:fill-foreground"
+        >
+          <PieChart width={300} height={300}>
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+            <Pie
+              data={data.filter((d) => d.value > 0)}
+              dataKey="value"
+              label={({ name, percent }) =>
+                `${name} ${(percent * 100).toFixed(0)}%`
+              }
+            />
+
+            <Legend />
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          {footerChartTitle}
+        </div>
+        <div className="leading-none text-muted-foreground">
+          {footerChartDescription}
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/pages/dashboard/murid/statistik/long-time/index.tsx
+++ src/pages/dashboard/murid/statistik/long-time/index.tsx
@@ -1,16 +1,58 @@
 import { PageContainer } from "@/components/layout/pageContainer";
 import { SectionContiner } from "@/components/layout/sectionContiner";
+import { ChartPieLabel } from "@/components/layout/chartPieLabel";
 import { api } from "@/utils/api";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function longTimeStatisticStudentPage() {
-  const { data } = api.student.GetAttendanceSummaryAllTime.useQuery();
+  const {
+    data: attendanceSummary,
+    isLoading,
+    error,
+  } = api.student.GetAttendanceSummaryAllTime.useQuery();
 
-  console.log(data);
+  if (isLoading) {
+    <Skeleton />;
+  }
+
+  const chartData = attendanceSummary
+    ? [
+        {
+          name: "Hadir Tepat",
+          value: attendanceSummary.hadirTepat,
+          fill: "#22c55e",
+        },
+        {
+          name: "Hadir Terlambat",
+          value: attendanceSummary.hadirTerlambat,
+          fill: "#4ade80",
+        },
+        { name: "Izin", value: attendanceSummary.izin, fill: "#f59e0b" },
+        { name: "Sakit", value: attendanceSummary.sakit, fill: "#3b82f6" },
+        { name: "Alpa", value: attendanceSummary.alpa, fill: "#ef4444" },
+        {
+          name: "Keluar Izin",
+          value: attendanceSummary.keluarIzin,
+          fill: "#a855f7",
+        },
+        {
+          name: "Keluar Tanpa Izin",
+          value: attendanceSummary.keluarTanpaIzin,
+          fill: "#f87171",
+        },
+      ]
+    : [];
 
   return (
     <PageContainer center variantBg={"secondary"}>
       <SectionContiner>
-        <div></div>
+        <ChartPieLabel
+          title="Attendance Summary"
+          description="All-time Attendance"
+          data={chartData}
+          footerChartTitle=""
+          footerChartDescription={`total hari: ${attendanceSummary?.totalHari}`}
+        />
       </SectionContiner>
     </PageContainer>
   );
